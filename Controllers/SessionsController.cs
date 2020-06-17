b67f3fc56956b5934bc34ea064b26ae4dff161ff
jtraviscoates <coatesjt@hotmail.com>
2020-06-17 10:11:15 -0500

    Create Add a Session From

---
+++ Controllers/SessionsController.cs
@@ -0,0 +1,26 @@
+﻿using MentorHub.Models;
+using MentorHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MentorHub.Controllers
+{
+    public class SessionsController : Controller
+    {
+        
+        private readonly ApplicationDbContext _context;
+        public SessionsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new SessionsFormViewModel
+            {
+                Profession =  _context.Occupations.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModels/SessionsFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MentorHub.Models;
+using System.Collections.Generic;
+
+namespace MentorHub.ViewModels
+{
+    public class SessionsFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; } 
+        public string Time { get; set; }
+        public int Occupations { get; set; }
+        public IEnumerable<Occupations> Profession { get; set; }
+
+    }
+}
